5ea6351d7a7f4aa1c31953fe01e29bacc5c8636e
shivraj tiwaskar <shivrajtiwaskar7@gmail.com>
2023-10-18 22:50:34 +0530

    Add project files.

---
+++ Controllers/newUserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+
+namespace feedback.Controllers
+{
+
+    public class newUserController : Controller
+    {
+        // GET: newUser
+        string _connectionString = ConfigurationManager.ConnectionStrings["UAIMS"].ConnectionString;
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult btnsubmit(string firstname, string Lastname, string Contactno, string email)
+        {
+            SqlConnection con = new SqlConnection(_connectionString);
+            SqlCommand cmd = new SqlCommand("NEW_REGISTER", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@FIRST_NAME", firstname);
+            cmd.Parameters.AddWithValue("@LAST_NAME", Lastname);
+            cmd.Parameters.AddWithValue("@CONTACT_NO", Contactno);
+            cmd.Parameters.AddWithValue("@EMAIL", email);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            return Json("");
+        }
+
+    }
+}
---
+++ Scripts/newUser/newUser.js
@@ -0,0 +1,53 @@
+﻿const FirstName = document.getElementById("txt_first_name");
+const LastName = document.getElementById("txt_last_name");
+const ContactNo = document.getElementById("txt_contact_no");
+const Email = document.getElementById("txt_email");
+
+function btnsubmit() {
+    if (isValidate(All_inputEle, '') == false) {
+        return
+    }
+    if (isValidate(All_inputEle, 'radio') == false) {
+        return
+    }
+
+    alert('signup')
+}
+
+
+const mainDiv = document.getElementById('signup_sec')
+
+//const All_inputEle = mainDiv.querySelectorAll('input:not([type="radio"])')
+const All_inputEle = mainDiv.querySelectorAll('input:not([type="radio"])')
+
+
+
+function isValidate(All_inputEle, flag) {
+    debugger
+    let valid = true;
+    if (flag == 'radio') {
+        // condition for checking radio button
+
+    } else {
+        All_inputEle.forEach(function (Curr_value, index) {
+
+            if (Curr_value.value == '') {
+                Curr_value.classList.add('border-danger')
+                valid = false
+            } else {
+                Curr_value.classList.remove('border-danger')
+                if (Curr_value.validationMessage != '') {
+                    Curr_value.classList.add('border-danger')
+                    Curr_value.nextElementSibling.innerHTML = Curr_value.validationMessage
+                    valid = false
+                } else {
+                    Curr_value.nextElementSibling.innerHTML = ''
+                }
+            }
+
+        })
+    }
+    return valid
+
+}
+
